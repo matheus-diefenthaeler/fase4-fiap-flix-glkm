5a1a1387731288bf6d2f03ee9a9d9f697a702477
Matheus Diefenthaeler <matheus.axo@gmail.com>
2024-01-25 18:05:28 -0300

    feat: get favorites

--- src/main/java/br/com/fiap/fase4streamingvideos/adapter/beanconfigs/UserConfiguration.java
+++ src/main/java/br/com/fiap/fase4streamingvideos/adapter/beanconfigs/UserConfiguration.java
@@ -2,18 +2,9 @@ package br.com.fiap.fase4streamingvideos.adapter.beanconfigs;
 
 import br.com.fiap.fase4streamingvideos.adapter.gateways.MongoDB.impl.user.UserCreationMongoDbGateway;
 import br.com.fiap.fase4streamingvideos.adapter.presenter.UserPresenter;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.ICreateUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.IDeleteUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.IFindAllUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.IFindByIdUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.output.IUserDeleteGateway;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.output.IUserFindByIdGateway;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.output.IUserFindGateway;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.output.IUserRegisterGateway;
-import br.com.fiap.fase4streamingvideos.application.user.interectors.CreateUserInteractor;
-import br.com.fiap.fase4streamingvideos.application.user.interectors.DeleteUserInteractor;
-import br.com.fiap.fase4streamingvideos.application.user.interectors.FindAllUserInteractor;
-import br.com.fiap.fase4streamingvideos.application.user.interectors.FindByIdUserInteractor;
+import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.*;
+import br.com.fiap.fase4streamingvideos.application.user.boundaries.output.*;
+import br.com.fiap.fase4streamingvideos.application.user.interectors.*;
 import br.com.fiap.fase4streamingvideos.application.user.presenter.IUserPresenter;
 import br.com.fiap.fase4streamingvideos.domain.factories.IUserFactory;
 import br.com.fiap.fase4streamingvideos.domain.factories.UserFactory;
@@ -56,6 +47,11 @@ public class UserConfiguration {
     @Bean
     public IFindByIdUserBoundary userFindByIdInputBoundary(IUserPresenter userPresenter, IUserFactory userFactory, IUserFindByIdGateway userFindByIdGateway) {
         return new FindByIdUserInteractor(userPresenter, userFactory, userFindByIdGateway);
+    }
+
+    @Bean
+    public IGetUserFavoritesBoundary getUserFavoritesBoundary(IUserPresenter userPresenter, IUserFactory userFactory, IGetUserFavoritesGateway getUserFavoritesGateway) {
+        return new GetUserFavoritesInteractor(userPresenter, userFactory, getUserFavoritesGateway);
     }
 
 }
--- src/main/java/br/com/fiap/fase4streamingvideos/adapter/controller/UserController.java
+++ src/main/java/br/com/fiap/fase4streamingvideos/adapter/controller/UserController.java
@@ -1,9 +1,6 @@
 package br.com.fiap.fase4streamingvideos.adapter.controller;
 
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.ICreateUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.IDeleteUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.IFindAllUserBoundary;
-import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.IFindByIdUserBoundary;
+import br.com.fiap.fase4streamingvideos.application.user.boundaries.input.*;
 import br.com.fiap.fase4streamingvideos.application.user.model.request.UserRequestModel;
 import br.com.fiap.fase4streamingvideos.application.user.model.response.UserResponseModel;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +13,18 @@ public class UserController {
 
     ICreateUserBoundary inputBoundary;
     IDeleteUserBoundary inputDeleteBoundary;
-
     IFindAllUserBoundary inputFindAllBoundary;
     IFindByIdUserBoundary inputFindByIdBoundary;
+    IGetUserFavoritesBoundary iGetUserFavoritesBoundary;
 
-    public UserController(ICreateUserBoundary inputBoundary, IDeleteUserBoundary inputDeleteBoundary, IFindAllUserBoundary inputFindAllBoundary, IFindByIdUserBoundary inputFindByIdBoundary) {
+    public UserController(ICreateUserBoundary inputBoundary, IDeleteUserBoundary inputDeleteBoundary,
+                          IFindAllUserBoundary inputFindAllBoundary, IFindByIdUserBoundary inputFindByIdBoundary,
+                          IGetUserFavoritesBoundary iGetUserFavoritesBoundary) {
         this.inputBoundary = inputBoundary;
         this.inputDeleteBoundary = inputDeleteBoundary;
         this.inputFindAllBoundary = inputFindAllBoundary;
         this.inputFindByIdBoundary = inputFindByIdBoundary;
+        this.iGetUserFavoritesBoundary = iGetUserFavoritesBoundary;
     }
 
     @PostMapping
@@ -33,7 +33,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{email}")
-    public Mono<Void> deleteById(@PathVariable String email) {
+    public Mono<Void> deleteByEmail(@PathVariable String email) {
         return this.inputDeleteBoundary.deleteByEmail(email);
     }
 
@@ -45,6 +45,12 @@ public class UserController {
     @GetMapping("/{id}")
     public Mono<UserResponseModel> findById(@PathVariable String id) {
         return inputFindByIdBoundary.findById(id);
+    }
+
+    @GetMapping("/{id}/favoritos")
+    public Flux<?> getFavorites(@PathVariable String id) {
+        return iGetUserFavoritesBoundary.getFavorites(id);
+
     }
 
 }
